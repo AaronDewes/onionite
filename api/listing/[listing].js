d59e9101ecd10491c33c4aabe6d68dfc3ddb755c
Aaron Dewes <aaron.dewes@web.de>
2020-09-23 21:15:52 +0200

    Test

--- api/listing/[listing].js
+++ api/listing/[listing].js
@@ -1,5 +1,24 @@
 const tor = require('../../lib/tor');
 
 module.exports = (req, res) => {
-  res.send(req.query)
+	let title = 'Top nodes by consensus weight';
+	const query = {
+		limit: 10
+	};
+	if (req.query.listing.startsWith("s")) {
+		title = `Search results for "${req.query.listing.substr(1)}":`;
+		query.search = req.query.listing.substr(1);
+	} else {
+		query.order = '-consensus_weight';
+		query.running = true;
+	}
+	if (req.query.listing.startsWith("p")) {
+		query.offset = (query.limit * req.query.listing.substr(1)) - query.limit;
+	}
+
+	tor.listNodes(query)
+		.then(nodes => {
+		res.send(nodes)
+		}
+		)
 };
